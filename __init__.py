ef321499e4af35d2a59c280164212a9183a5064b
XavierCLL <xavier.corredor.llano@gmail.com>
2021-10-06 23:31:29 -0700

    Remove all extra deps code not used

--- __init__.py
+++ __init__.py
@@ -19,35 +19,6 @@
  ***************************************************************************/
 """
 
-from qgis.PyQt.QtCore import QCoreApplication
-from qgis.PyQt.QtWidgets import QMessageBox
-
-from Coregistration.utils.extra_deps import load_install_extra_deps, WaitDialog
-
-
-def pre_init_plugin(iface):
-    app = QCoreApplication.instance()
-    parent = iface.mainWindow()
-    dialog = None
-    log = ''
-    try:
-        for msg_type, msg_val in load_install_extra_deps():
-            app.processEvents()
-            if msg_type == 'log':
-                log += msg_val
-            elif msg_type == 'needs_install':
-                dialog = WaitDialog(parent, 'Coregistration - installing dependencies')
-            elif msg_type == 'install_done':
-                dialog.accept()
-    except Exception as e:
-        if dialog:
-            dialog.accept()
-        QMessageBox.critical(parent, 'Coregistration - installing dependencies',
-                             'An error occurred during the installation of Python packages. ' +
-                             'Click on "Stack Trace" in the QGIS message bar for details.')
-        raise RuntimeError('\nCoregistration: Error installing Python packages. Read install instruction: '
-                           'https://github.com/SMByC/Coregistration-Qgis-processing\nLog:\n' + log) from e
-
 
 # noinspection PyPep8Naming
 def classFactory(iface):  # pylint: disable=invalid-name
@@ -56,8 +27,6 @@ def classFactory(iface):  # pylint: disable=invalid-name
     :param iface: A QGIS interface instance.
     :type iface: QgsInterface
     """
-    # load/install extra python dependencies
-    # pre_init_plugin(iface)
 
     #
     from Coregistration.Coregistration_plugin import CoregistrationPlugin
