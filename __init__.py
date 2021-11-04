4d7a31cfbf7ec393fd1be7b9d06c4800b6478154
XavierCLL <xavier.corredor.llano@gmail.com>
2021-11-04 12:54:45 -0700

    Added libs for install extra depends and added Arosics as a plugin depend

--- __init__.py
+++ __init__.py
@@ -18,7 +18,34 @@
  *                                                                         *
  ***************************************************************************/
 """
+from qgis.PyQt.QtCore import QCoreApplication
+from qgis.PyQt.QtWidgets import QMessageBox
 
+from Coregistration.utils.extra_deps import load_install_extra_deps, WaitDialog
+
+
+def pre_init_plugin(iface):
+    app = QCoreApplication.instance()
+    parent = iface.mainWindow()
+    dialog = None
+    log = ''
+    try:
+        for msg_type, msg_val in load_install_extra_deps():
+            app.processEvents()
+            if msg_type == 'log':
+                log += msg_val
+            elif msg_type == 'needs_install':
+                dialog = WaitDialog(parent, 'Co-Registration - installing dependencies')
+            elif msg_type == 'install_done':
+                dialog.accept()
+    except Exception as e:
+        if dialog:
+            dialog.accept()
+        QMessageBox.critical(parent, 'Co-Registration - installing dependencies',
+                             'An error occurred during the installation of Python packages. ' +
+                             'Click on "Stack Trace" in the QGIS message bar for details.')
+        raise RuntimeError('\nCo-Registration: Error installing Python packages. Read install instruction: '
+                           'https://github.com/SMByC/Coregistration-Qgis-processing\nLog:\n' + log) from e
 
 # noinspection PyPep8Naming
 def classFactory(iface):  # pylint: disable=invalid-name
@@ -27,6 +54,8 @@ def classFactory(iface):  # pylint: disable=invalid-name
     :param iface: A QGIS interface instance.
     :type iface: QgsInterface
     """
+    # load/install extra python dependencies
+    pre_init_plugin(iface)
 
     #
     from Coregistration.coregistration_plugin import CoregistrationPlugin
